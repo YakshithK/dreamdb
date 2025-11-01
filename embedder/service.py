5a43e8227745c2d30af985f55239688525f52531
Yakshith Kommineni <Yakshith.kommineni@gmail.com>
2025-11-01 02:17:09 -0400

    changed to smaller model

--- embedder/service.py
+++ embedder/service.py
@@ -15,7 +15,7 @@ def get_model():
     global model
     if model is None:
         print("Loading model...")
-        model = SentenceTransformer("all-MiniLM-L6-v2")
+        model = SentenceTransformer("sentence-transformers/paraphrase-MiniLM-L3-v2")
         print("Model loaded!")
     return model
 
