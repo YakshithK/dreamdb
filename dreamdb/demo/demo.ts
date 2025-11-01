291714e5c8e0510cb66212121d0f730fe803c58f
Yakshith Kommineni <Yakshith.kommineni@gmail.com>
2025-10-31 23:14:14 -0400

    fixed issues + database not dataset

--- dreamdb/demo/demo.ts
+++ dreamdb/demo/demo.ts
@@ -1,7 +1,7 @@
 import { DreamDB } from "../src/index.js";
 
 async function demo() {
-  const db = await DreamDB.connect({ connectionString: "./demo/dataset.sqlite" });
+  const db = await DreamDB.connect({ connectionString: "./demo/database.sqlite" });
 
   // Insert new row
   await db.insert("users", { name: "Charlie", last_active: "2025-10-10", notes: "Paused 2 months" });
--- dreamdb/src/dbConnector.ts
+++ dreamdb/src/dbConnector.ts
@@ -1,4 +1,4 @@
-import { Database } from "better-sqlite3";
+import Database from "better-sqlite3";
 import crypto from "crypto";
 
 export class DBConnector { 
